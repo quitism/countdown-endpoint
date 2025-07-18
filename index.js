fc243e3028556faaae7b8a35d520e594a40e36d0
quitism <quitism69@gmail.com>
2025-07-18 13:40:15 +0700

    idk what im doing

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const viewers = require('./api/viewers');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins, change if needed
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Handle OPTIONS preflight
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204); // No Content
+    }
+
+    next();
+});
+
 app.use(express.json());
 app.use('/api/viewers', viewers);
 
