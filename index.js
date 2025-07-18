610935c77889a34a0289022c8388457438968f9d
quitism <quitism69@gmail.com>
2025-07-18 13:29:42 +0700

    changed fallback port to 8000
    simple change i know

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const viewers = require('./api/viewers');
 app.use(express.json());
 app.use('/api/viewers', viewers);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Viewer API running on port ${PORT}`);
 });
